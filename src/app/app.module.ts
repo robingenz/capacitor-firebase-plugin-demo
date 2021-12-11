5a59f8046cd5dfe1003232cd87f5381ded8b5a49
Robin Genz <mail@robingenz.dev>
2021-12-11 21:30:29 +0100

    ng update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,15 @@ import { AppComponent } from './app.component';
 import { GlobalErrorHandlerService } from './core';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    {
-      provide: ErrorHandler,
-      useClass: GlobalErrorHandlerService,
-    },
-  ],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        {
+            provide: ErrorHandler,
+            useClass: GlobalErrorHandlerService,
+        },
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
