e1caa85fa145b346a1833e17e266ec503493dded
Robin Genz <mail@robingenz.dev>
2022-05-05 14:35:31 +0200

    working

--- src/app/modules/firebase-messaging/firebase-messaging.page.ts
+++ src/app/modules/firebase-messaging/firebase-messaging.page.ts
@@ -1,7 +1,6 @@
 import { Component, NgZone, OnInit } from '@angular/core';
 import { FirebaseMessaging } from '@capacitor-firebase/messaging';
 import { environment } from '@env/environment';
-import { getMessaging, onMessage } from 'firebase/messaging';
 
 const LOGTAG = '[FirebaseMessagingPage]';
 
@@ -16,47 +15,38 @@ export class FirebaseMessagingPage implements OnInit {
   private readonly githubUrl =
     'https://github.com/robingenz/capacitor-firebase';
 
-  constructor(private readonly ngZone: NgZone) {}
-
-  public ngOnInit(): void {
-    FirebaseMessaging.addListener('tokenReceived', (event) => {
-      console.log(`${LOGTAG} tokenReceived`, { event });
-      this.ngZone.run(() => {
-        this.token = event.token;
+  constructor(private readonly ngZone: NgZone) {
+    FirebaseMessaging.removeAllListeners().then(() => {
+      FirebaseMessaging.addListener('tokenReceived', (event) => {
+        console.log(`${LOGTAG} tokenReceived`, { event });
+        this.ngZone.run(() => {
+          this.token = event.token;
+        });
+      });
+      FirebaseMessaging.addListener('notificationReceived', (event) => {
+        console.log(`${LOGTAG} notificationReceived`, { event });
+      });
+      FirebaseMessaging.addListener('notificationActionPerformed', (event) => {
+        console.log(`${LOGTAG} notificationActionPerformed`, { event });
       });
     });
-    FirebaseMessaging.addListener('notificationReceived', (event) => {
-      console.log(`${LOGTAG} notificationReceived`, { event });
+  }
+
+  public ngOnInit(): void {
+    FirebaseMessaging.getToken({
+      vapidKey: environment.firebase.vapidKey,
+    }).then((result) => {
+      this.token = result.token;
     });
-    FirebaseMessaging.addListener('notificationActionPerformed', (event) => {
-      console.log(`${LOGTAG} notificationActionPerformed`, { event });
-    });
-    const messaging = getMessaging();
-    onMessage(messaging, (payload) => console.log(payload));
   }
 
   public openOnGithub(): void {
     window.open(this.githubUrl, '_blank');
   }
 
-  public async requestPermissions(): Promise<void> {
-    await FirebaseMessaging.requestPermissions();
-  }
+  public async requestPermissions(): Promise<void> {}
 
-  public async getToken(): Promise<void> {
-    const serviceWorkerRegistration = await navigator.serviceWorker.register(
-      'firebase-messaging-sw.js',
-      { type: 'module' }
-    );
-    const { token } = await FirebaseMessaging.getToken({
-      vapidKey: environment.firebase.vapidKey,
-      serviceWorkerRegistration,
-    });
-    this.token = token;
-  }
+  public async getToken(): Promise<void> {}
 
-  public async deleteToken(): Promise<void> {
-    await FirebaseMessaging.deleteToken();
-    this.token = '';
-  }
+  public async deleteToken(): Promise<void> {}
 }
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,11 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
-import { onBackgroundMessage } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging-sw.js";
-import {
-  getMessaging,
-  isSupported,
-} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging.js";
+// Import and configure the Firebase SDK
+// These scripts are made available when the app is served or deployed on Firebase Hosting
+// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.7.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging-compat.js"
+);
 
-const firebaseApp = initializeApp({
+firebase.initializeApp({
   apiKey: "AIzaSyAdSJa6qYSkHVQIkPGkl0OtDu8w2JbTwe8",
   authDomain: "capacitor-firebase-plugin-demo.firebaseapp.com",
   projectId: "capacitor-firebase-plugin-demo",
@@ -14,21 +17,55 @@ const firebaseApp = initializeApp({
   appId: "1:20043654228:web:98b28eaef48dad770939c1",
   measurementId: "G-K2DPKN6HD5",
 });
+const messaging = firebase.messaging();
 
-isSupported().then((isSupported) => {
-  console.log({ isSupported });
-  if (isSupported) {
-    const messaging = getMessaging(firebaseApp);
+/**
+ * Here is is the code snippet to initialize Firebase Messaging in the Service
+ * Worker when your app is not hosted on Firebase Hosting.
 
-    onBackgroundMessage(
-      messaging,
-      ({ notification: { title, body, image } }) => {
-        console.log("onBackgroundMessage", { notification });
-        self.registration.showNotification(title, {
-          body,
-          icon: image || "/assets/icon/favicon.png",
-        });
-      }
-    );
-  }
+ // Give the service worker access to Firebase Messaging.
+ // Note that you can only use Firebase Messaging here. Other Firebase libraries
+ // are not available in the service worker.
+ importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
+ importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
+
+ // Initialize the Firebase app in the service worker by passing in
+ // your app's Firebase config object.
+ // https://firebase.google.com/docs/web/setup#config-object
+ firebase.initializeApp({
+   apiKey: 'api-key',
+   authDomain: 'project-id.firebaseapp.com',
+   databaseURL: 'https://project-id.firebaseio.com',
+   projectId: 'project-id',
+   storageBucket: 'project-id.appspot.com',
+   messagingSenderId: 'sender-id',
+   appId: 'app-id',
+   measurementId: 'G-measurement-id',
+ });
+
+ // Retrieve an instance of Firebase Messaging so that it can handle background
+ // messages.
+ const messaging = firebase.messaging();
+ **/
+
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// Keep in mind that FCM will still show notification messages automatically
+// and you should use data messages for custom notifications.
+// For more info see:
+// https://firebase.google.com/docs/cloud-messaging/concept-options
+messaging.onBackgroundMessage(function (payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = "Background Message Title";
+  const notificationOptions = {
+    body: "Background Message body.",
+    icon: "/firebase-logo.png",
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
